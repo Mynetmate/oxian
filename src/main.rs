6a20457482ce41f99100367cabe3fe1eb7ab1e59
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2026-08-02 20:25:51 +0700

    chore: add snmp todo

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,18 @@ fn snmp_scan(ip_cidr: Ipv4Net) {
     let ip_network = ip_cidr.network();
 
     if ip != ip_network {
-        // scan only router or switch
+        snmp_walk();
+        todo!("scan only router or switch");
     } else {
-        // scan all network
+        snmp_walk();
+        todo!("scan all network");
     }
+}
+
+fn snmp_scan_agent() {
+    todo!("scan snmp agent");
+}
+
+fn snmp_walk() {
+    todo!("scan snmp agent in other networks");
 }
